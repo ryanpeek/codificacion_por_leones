7d8eff65aa083f94a2f1fbf1a8f3cd5b98040d38
Ryan Peek <ryanpeek@users.noreply.github.com>
2020-12-18 16:37:21 -0800

    ryan adds a few lines in Github.com

--- scripts/working_with_github.R
+++ scripts/working_with_github.R
@@ -9,3 +9,6 @@
 ## 1. We stage a file by clicking the box and adding it.
 ## 2. We commit the file, and type a message in the commit box.
 ## 3. We push that up to the cloud where the repository lives.
+
+
+## 4. Ryan adds some code online in the repo and saves it online. We need to PULL those changes to our local copy on our computer.
