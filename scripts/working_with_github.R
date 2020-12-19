da388f2ded8b6a64c7b9be5326ea5a9e4fabe437
Ryan Peek <rapeek@ucdavis.edu>
2020-12-18 16:33:41 -0800

    add some code to git code

--- scripts/working_with_github.R
+++ scripts/working_with_github.R
@@ -2,3 +2,10 @@
 
 # good resources to know:
 ## https://happygitwithr.com/
+
+
+# STEPS TO USING GIT ------------------------------------------------------
+
+## 1. We stage a file by clicking the box and adding it.
+## 2. We commit the file, and type a message in the commit box.
+## 3. We push that up to the cloud where the repository lives.
